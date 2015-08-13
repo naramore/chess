4d39350214fbd5c82cd54f10da59ab168c4b3610
naramore <naramore@gmail.com>
2015-08-12 21:27:50 -0500

    refactored board-state...
    player-state, and game-log into game-state

--- src/chess/core.clj
+++ src/chess/core.clj
@@ -17,54 +17,39 @@
 ;;    | a b c d e f g h |
 ;; Light (lowercase characters)
 
-(def starting-board-state [\R \N \B \K \Q \B \N \R
-                           \P \P \P \P \P \P \P \P
-                           \- \- \- \- \- \- \- \-
-                           \- \- \- \- \- \- \- \-
-                           \- \- \- \- \- \- \- \-
-                           \- \- \- \- \- \- \- \-
-                           \p \p \p \p \p \p \p \p
-                           \r \n \b \k \q \b \n \r])
+(def starting-board [\R \N \B \K \Q \B \N \R
+                     \P \P \P \P \P \P \P \P
+                     \- \- \- \- \- \- \- \-
+                     \- \- \- \- \- \- \- \-
+                     \- \- \- \- \- \- \- \-
+                     \- \- \- \- \- \- \- \-
+                     \p \p \p \p \p \p \p \p
+                     \r \n \b \k \q \b \n \r])
 
-(def board-state (atom nil))
-(def board-history (atom nil))
-(def player-state (atom nil))
-(def game-log (atom nil))
+(def game-state (atom nil))
+(def game-history (atom nil))
 
-(add-watch board-state :history
+(add-watch game-state :history
     (fn [_ _ _ n]
-        (when-not (= (last @board-history) n)
-            (swap! board-history conj n))))
-
-(defn- record! [pos dest]
-    (swap! game-log conj [pos dest]))
-
-(defn- turn-complete! []
-    (cond
-        (= @player-state :light) (reset! player-state :dark)
-        (= @player-state :dark) (reset! player-state :light)
-        :else nil))
+        (when-not (= (last @game-history) n)
+            (swap! game-history conj n))))
 
 (defn undo! []
-    (when (> (count @board-history) 1)
-        (swap! board-history pop)
-        (swap! game-log pop)
-        (reset! board-state (last @board-history))))
+    (when (> (count @game-history) 1)
+        (swap! game-history pop)
+        (reset! game-state (last @game-history))))
 
 (defn start-game! []
-    (do (reset! game-log [])
-        (reset! player-state :light)
-        (reset! board-state starting-board-state)
-        (reset! board-history [@board-state])))
+    (do (reset! game-state {:board starting-board
+                            :player :light
+                            :log []})
+        (reset! game-history [@game-state])))
 
 (defn stop-game! []
-    (do (reset! game-log nil)
-        (reset! player-state nil)
-        (reset! board-state nil)
-        (reset! board-history nil)))
+    (do (reset! game-state nil)
+        (reset! game-history nil)))
 
 (defn move! [pos dest]
-    (let [result (normal-move! board-state player-state pos dest)]
+    (let [result (normal-move! game-state pos dest)]
         (cond (not-nil? result)
-              (do (record! pos dest)
-                  (turn-complete!)))))
+              result)))
--- src/chess/move.clj
+++ src/chess/move.clj
@@ -2,13 +2,6 @@
     (:require [chess.indexer :refer [lookup index contains-value?]]
               [chess.move.validation :refer [get-player-moves]]))
 
-(defn normal-move! [board-atom player-atom pos dest]
-    (let [piece (lookup @board-atom pos)]
-        (if (contains-value? (get-player-moves @board-atom @player-atom) [pos dest])
-        	(swap! board-atom assoc (index pos) \-
-                                    (index dest) piece)
-            nil)))
-
 (defn castle! [king-pos rook-pos])
 
 (defn en-passant! [pos enemy-pos])
@@ -18,3 +11,23 @@
 (defn has-moved? [board-history pos]
     (->> (map #(lookup % pos) board-history)
          (apply =)))
+
+(defn next-player [current-player]
+	(cond
+		(= current-player :light) :dark
+		(= current-player :dark) :light
+		:else nil))
+
+(defn update-game [game pos dest]
+	(let [piece (lookup (game :board) pos)]
+		(-> (assoc-in game [:board (index pos)] \-)
+			(assoc-in [:board (index dest)] piece)
+			(assoc :player (next-player (game :player)))
+			(assoc-in [:log (count (game :log))] [pos dest]))))
+
+(defn normal-move! [game-atom pos dest]
+    (let [board (@game-atom :board)
+    	  player (@game-atom :player)]
+        (if (contains-value? (get-player-moves board player) [pos dest])
+        	(swap! game-atom update-game pos dest)
+            nil)))
