797abf0f2eb3c764d69b9c2a30954a3d217a5a08
naramore <naramore@gmail.com>
2015-08-13 20:24:57 -0500

    Updated print-board to include labels.

--- src/chess/print.clj
+++ src/chess/print.clj
@@ -5,23 +5,27 @@
 (def board-hight (count (valid-ranks)))
 (def board-size (* board-width board-hight))
 
-(def generate-board-edge
-    (->> (repeat board-width "---|")
-         (apply concat)
-         (concat "|")
-         (apply str)))
+(defn generate-board-edge
+    ([] (generate-board-edge "---"))
+    ([s] (->> (repeat board-width (str s "|"))
+              (apply str)
+              (str "  |"))))
 
-(defn generate-board-rank [rank]
-    (->> (mapcat #(str " " (if (= % \-) \space %) " |") rank)
-         (concat "|")
-         (apply str)))
+(defn generate-board-rank
+    ([rank] (generate-board-rank nil rank))
+    ([s rank] (generate-board-rank s "|" rank))
+    ([s div rank]
+        (->> (mapcat #(str " " (if (= % \-) \space %) " " div) rank)
+             (concat s div)
+             (apply str))))
 
 (defn generate-board [board]
     (->> (partition board-width board)
-         (map generate-board-rank)
-         (interpose generate-board-edge)
-         (cons generate-board-edge)
-         (#(concat % [generate-board-edge]))
+         (map generate-board-rank (map #(str % " ") (reverse (valid-ranks))))
+         (interpose (generate-board-edge))
+         (cons (generate-board-edge))
+         (#(concat % [(generate-board-edge)
+                      (generate-board-rank "  " " " (valid-files))]))
          ((partial clojure.string/join "\n"))))
 
 (defn print-board [board]
